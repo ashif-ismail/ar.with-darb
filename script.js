214548d9a6fbded12a07a658b7aa6ff2bd8c1322
Ashif <cyber2maro@gmail.com>
2020-09-09 11:55:35 +0400

    modified a-text

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
                    
         let model = document.createElement('a-text');
-        model.setAttribute('value', "AR on Darb");
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('value', "Al Khor Street/DOT");
 
         scene.appendChild(model);
     });
