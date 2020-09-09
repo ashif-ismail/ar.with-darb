abf686886dd0f01caa4aafa6df5a73eb6babe1f8
Ashif <cyber2maro@gmail.com>
2020-09-09 12:50:28 +0400

    modified a-text

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
                    
         let model = document.createElement('a-bustime');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         //model.setAttribute('value', "Al Khor Street/DOT");
 
         scene.appendChild(model);
