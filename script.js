8011db29c83e3ba206ea494a2102193778888ec4
Ashif <cyber2maro@gmail.com>
2020-09-09 12:14:38 +0400

    modified a-text

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function renderPlaces(places) {
 
         scene.appendChild(model);
         scene.appendChild(model1);
-        scene.appendChild(mode2);
+        scene.appendChild(model2);
         scene.appendChild(model3);
         scene.appendChild(model4);
         scene.appendChild(model5);
