078ad87f2181c4c13de6e2382bbe9d4dc6aae4a0
Ashif <cyber2maro@gmail.com>
2020-09-10 14:23:51 +0400

    add pwa support and removed assets folder

---
+++ bustime.js
@@ -0,0 +1,70 @@
+AFRAME.registerComponent('a-bustime', {
+    schema: {
+        'target': {
+            type: 'selector'
+        },
+    },
+    init: function() {
+        let entity = document.querySelector('a-entity');
+        entity.setAttribute("visible",false);
+        navigator.geolocation.getCurrentPosition(function(position) {
+            //alert("user position :" + position.coords.latitude + "," + position.coords.longitude);
+            entity.setAttribute("visible",true);
+            
+            const stopNameText = document.createElement('span');
+            stopNameText.id = "stopNameText"
+            stopNameText.style.cssText = 'font-size: 40px;';
+            stopNameText.innerText = "Al Khor St/DOT 097171-A";
+            entity.appendChild(stopNameText);
+                                                 
+            const dividerDiv = document.createElement('div');
+            dividerDiv.style.cssText = 'width:100%;background-color:rgba(0,0,0,0.2);height: 10px;border-radius:50px;';
+            entity.appendChild(dividerDiv);
+                                                 
+//            const br = document.createElement('br');
+//            entity.appendChild(br);
+                                                 
+            const containerDiv = document.createElement('div');
+            containerDiv.style.cssText = 'text-align:start;';
+            entity.appendChild(containerDiv);
+                                                 
+            var i;
+            for (i = 0; i <= 10; i++) {
+                                                 
+                const lineNoText = document.createElement('span');
+                lineNoText.id = "lineNoText"
+                lineNoText.style.cssText = 'font-size: 20px;';
+                lineNoText.innerText = "Line 022   ";
+                containerDiv.appendChild(lineNoText);
+                                                 
+                const towardsText = document.createElement('span');
+                towardsText.id = "towardsText"
+                towardsText.style.cssText = 'font-size: 20px;';
+                towardsText.innerText = "towards";
+                containerDiv.appendChild(towardsText);
+
+                const destinationText = document.createElement('span');
+                destinationText.id = "destinationText"
+                destinationText.style.cssText = 'font-size: 24px;';
+                destinationText.innerText = "Marina Mall";
+                containerDiv.appendChild(destinationText);
+
+                const timeText = document.createElement('span');
+                timeText.id = "timeText"
+                timeText.style.cssText = 'font-size: 24px;';
+                timeText.innerText = " on 7:44 AM";
+                containerDiv.appendChild(timeText);
+
+                const statusText = document.createElement('span');
+                statusText.id = "statusText"
+                statusText.style.cssText = 'font-size: 24px;';
+                statusText.innerText = " LIVE";
+                containerDiv.appendChild(statusText);
+
+                const br = document.createElement('br');
+                containerDiv.appendChild(br);
+            }
+        });
+
+    }
+});
--- script.js
+++ script.js
@@ -24,10 +24,23 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
                    
         let model = document.createElement('a-entity');
-        model.setAttribute('htmlembed','');
+    
+        model.setAttribute('htmlembed','ppu:256');
+        model.setAttribute('position','{x: 0, y: 2.5, z: -5}');
+        model.setAttribute('id','main');
+        model.setAttribute('class','dark main');
+        
+        const container = document.querySelector('div');
+        container.setAttribute('id', 'page1');
+        const label = document.createElement('h1');
+        label.innerText = "test";
+        model.appendChild(label);
+        document.body.appendChild(container);
+                   
         
         //model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        // model.setAttribute('value', "Al Khor Street/DOT");
+        
         
         scene.appendChild(model);
                    
